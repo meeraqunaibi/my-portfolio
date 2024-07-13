b565c46505f19b05c51a9324d71ab48303c72775
meera-iyad <191128@ppu.edu.ps>
2024-07-13 19:58:56 +0300

    add back button

--- src/pages/viewProject/viewProject.component.js
+++ src/pages/viewProject/viewProject.component.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import './viewProject.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 
 const ViewProject = (props) => {
     const [oneProject, setOneProject] = useState(null);
     const id = useParams();
+    const navigate = useNavigate();
     useEffect(() => {
         const currentProject =
             props.projects.find((p) => p.key === id.id) || null;
         setOneProject(currentProject);
         console.log(currentProject);
     }, [oneProject, props, id.id]);
+    const backAction = () => {
+        navigate("/");
+    }
     return (
         <div className='project-det'>
-            <h1>{oneProject?.title}</h1>
+            <div className='project-header'>
+                <FontAwesomeIcon cursor={true} icon={faArrowLeftLong} size='4x' onClick={backAction} />
+                <h1>{oneProject?.title}</h1>
+            </div>
             <p>
                 {oneProject?.desc}
             </p>
